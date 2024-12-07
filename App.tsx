b6009064d0674c0181a03e46ebceb72ee1f18399
Om chaudhari <omchaudhari0003@gmail.com>
2024-12-07 22:38:05 +0530

    Tab Icon

--- App.tsx
+++ App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Navigation from '@navigation/Navigation';
 import '@unistyles/unistyles';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor, store} from '@states/store';
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Navigation />;
+      </PersistGate>
+    </Provider>
+  );
 }
--- src/features/tabs/CustomTabBar.tsx
+++ src/features/tabs/CustomTabBar.tsx
@@ -1,12 +1,102 @@
-import {View, Text} from 'react-native';
+/* eslint-disable react/self-closing-comp */
 import React, {FC} from 'react';
 import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+import {useAppSelector} from '@states/reduxHook';
+import {useSharedState} from './SharedContext';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useStyles} from 'react-native-unistyles';
+import {tabStyles} from '@unistyles/tabStyles';
+import Animated, {useAnimatedStyle, withTiming} from 'react-native-reanimated';
+import {Colors} from '@unistyles/Constants';
+import {View} from 'react-native';
+import ScalePress from '@components/ui/ScalePress';
+import {
+  DeliveryTabIcon,
+  DiningTabIcon,
+  LiveTabIcon,
+  ReorderTabIcon,
+} from './TabsIcon';
 
-const CustomTabBar: FC<BottomTabBarProps> = () => {
+const CustomTabBar: FC<BottomTabBarProps> = props => {
+  const isVegMode = useAppSelector(state => state.user.isVegMode);
+  const {scrollY} = useSharedState();
+
+  const {state, navigation} = props;
+  const bottom = useSafeAreaInsets();
+
+  const {styles} = useStyles(tabStyles);
+  const isLiveTabFocused = state.routes[state.index]?.name === 'Live';
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: scrollY.value
+            ? withTiming(100, {duration: 300})
+            : withTiming(0, {duration: 300}),
+        },
+      ],
+    };
+  });
+
   return (
-    <View>
-      <Text>CustomTabBar</Text>
-    </View>
+    <>
+      <Animated.View
+        style={[
+          styles.tabBarContainer,
+          animatedStyle,
+          {
+            paddingBottom: bottom.bottom,
+            backgroundColor: isLiveTabFocused ? Colors.dark : Colors.background,
+          },
+        ]}>
+        <View style={styles.tabContainer}>
+          {state?.routes?.map((route, index) => {
+            const isFocused = state.index === index;
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route?.key,
+                canPreventDefault: true,
+              });
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route?.name);
+              }
+            };
+
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key,
+              });
+            };
+
+            return (
+              <ScalePress
+                onPress={onPress}
+                onLongPress={onLongPress}
+                key={index}
+                style={[
+                  styles.tabItem,
+                  isFocused ? styles.focusedTabItem : {},
+                ]}>
+                {route?.name === 'Delivery' && (
+                  <DeliveryTabIcon focused={isFocused} />
+                )}
+                {route?.name === 'Reorder' && (
+                  <ReorderTabIcon focused={isFocused} />
+                )}
+                {route?.name === 'Dining' && (
+                  <DiningTabIcon focused={isFocused} />
+                )}
+                {route?.name === 'Live' && <LiveTabIcon focused={isFocused} />}
+              </ScalePress>
+            );
+          })}
+          <View style={styles.verticalLine} />
+        </View>
+      </Animated.View>
+    </>
   );
 };
 
